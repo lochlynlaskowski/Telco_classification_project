3957beab411a9866953b663ef0f365d12c2550de
lochlynlaskowski <lochlyn.laskowski@gmail.com>
2022-05-24 14:35:45 -0500

    added additional work

--- prepare.py
+++ prepare.py
@@ -6,12 +6,16 @@ from sklearn.model_selection import train_test_split
 import acquire
 
 ########## TELCO DATA ###########
-def prep_telco(df):
-    df['total_charges'] = df['total_charges'].str.strip()
-    df = df[df.total_charges != '']
-    df['total_charges'] = df.total_charges.astype(float)
-    columns_to_drop = ['Unnamed: 0','internet_service_type_id', 'payment_type_id', 'contract_type_id']
-    df = df.drop(columns = columns_to_drop)
+def clean_telco_data(df):
+    # replace empty cells with nulls
+    df = df.replace(' ', np.nan)
+    # drop 11 nulls from total_charges
+    df.dropna(inplace=True)
+    # change total_charges to float
+    df['total_charges'] = pd.to_numeric(df['total_charges'])
+    # drop unnecessary columns
+    df = df.drop(columns=['Unnamed: 0', 'payment_type_id', 'internet_service_type_id', 'contract_type_id'])
+    # convert categorical columns to boolean
     df['is_female'] = df['gender'].map({'Female': 1, 'Male': 0})
     df['has_partner'] = df['partner'].map({'Yes': 1, 'No': 0})
     df['has_dependents'] = df['dependents'].map({'Yes': 1, 'No': 0})
@@ -19,14 +23,26 @@ def prep_telco(df):
     df['has_paperless_billing'] = df['paperless_billing'].map({'Yes': 1, 'No': 0})
     df['has_churned'] = df['churn'].map({'Yes': 1, 'No': 0})
     
-    dummy_df = pd.get_dummies(df[['gender', 'partner', 'phone_service', \
-                                  'multiple_lines', 'online_security', \
-                                  'online_backup', 'tech_support', \
-                                  'streaming_tv', 'streaming_movies', \
-                                  'paperless_billing', 'churn', 'contract_type', \
-                                  'internet_service_type', 'payment_type']], \
-                              dummy_na=False, drop_first=False)
+    # encode categorical columns 
+    dummy_df = pd.get_dummies(df[['contract_type','internet_service_type', 'payment_type',\
+                              'multiple_lines', 'online_security', 'online_backup',\
+                              'tech_support', 'streaming_tv', 'streaming_movies']],dummy_na=False, drop_first=True)
+    #combine dataframes
     df = pd.concat([df, dummy_df], axis=1)
+    
     return df
+
+
+def split_telco_data(df):
+
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=123, 
+                                        stratify=df.churn)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                   random_state=123, 
+                                   stratify=train_validate.churn)
+    
+    return train, validate, test
+    
 
     
